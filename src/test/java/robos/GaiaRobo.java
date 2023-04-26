b6533d0cf29f9ab331877b72f507868d963fa62c
Henrique-Arau <penielh43@gmail.com>
2023-04-26 14:53:13 -0300

    Iniciando ROBÔ

---
+++ src/test/java/robos/GaiaRobo.java
@@ -0,0 +1,16 @@
+package robos;
+
+public class GaiaRobo {
+
+    public void fazerLogin() {
+        System.out.println("Vou fazer login!");
+    }
+
+    public void lancarNota() {
+        System.out.println("vou lançar uma nota!");
+    }
+
+    public void abrirChrome() {
+        System.out.println("Estou abrindo o Chrome!");
+    }
+}
